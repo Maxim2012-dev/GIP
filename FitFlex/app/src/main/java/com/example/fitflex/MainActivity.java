0669173bd1ff5e2de8a7ac8df69e0b2fde99f01f
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2019-10-28 17:11:28 +0100

    login screen

--- FitFlex/app/src/main/java/com/example/fitflex/MainActivity.java
+++ FitFlex/app/src/main/java/com/example/fitflex/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.fitflex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView registerLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        registerLink = findViewById(R.id.registerLink);
+        SpannableString registreer = new SpannableString("Nog geen account? Registreer");
+        registreer.setSpan(new UnderlineSpan(), 18, 28, 0);
+        registerLink.setText(registreer);
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
