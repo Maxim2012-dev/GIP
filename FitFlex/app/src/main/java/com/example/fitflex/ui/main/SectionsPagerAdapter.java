64db1bd4cd86bfbd344097505c77a13fe1298761
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2020-02-09 09:24:19 +0100

    TabLayout KLAAR

---
+++ FitFlex/app/src/main/java/com/example/fitflex/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,66 @@
+package com.example.fitflex.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.fitflex.Frag1;
+import com.example.fitflex.Frag2;
+import com.example.fitflex.Frag3;
+import com.example.fitflex.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+
+        switch (position) {
+            case 0:
+                fragment = new Frag1();
+                break;
+            case 1:
+                fragment = new Frag2();
+                break;
+            case 2:
+                fragment = new Frag3();
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Frag1";
+            case 1:
+                return "Frag2";
+            case 2:
+                return "Frag3";
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
