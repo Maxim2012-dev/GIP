7ee5ea4a84e1caff7b484bd58f495b82e39e0cbc
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2020-02-28 00:20:28 +0100

    layout_profiel

---
+++ FitFlex/app/src/main/java/com/example/fitflex/ProfielActivity.java
@@ -0,0 +1,16 @@
+package com.example.fitflex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfielActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profiel);
+    }
+
+
+}
