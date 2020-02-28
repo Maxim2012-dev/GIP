8fbbe667fb886236fe70a9b2d063d84fb07b7654
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2020-02-28 11:19:11 +0100

    referentie_naar_profielActivity

--- FitFlex/app/src/main/java/com/example/fitflex/HomeActivity.java
+++ FitFlex/app/src/main/java/com/example/fitflex/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.fitflex;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,31 +10,35 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.example.fitflex.ui.main.SectionsPagerAdapter;
 
 public class HomeActivity extends AppCompatActivity {
 
+    FloatingActionButton floatingActionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        floatingActionButton = findViewById(R.id.fab);
+
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(getApplicationContext(), ProfielActivity.class));
             }
         });
     }
+
+
+
 }
--- FitFlex/app/src/main/java/com/example/fitflex/ProfielActivity.java
+++ FitFlex/app/src/main/java/com/example/fitflex/ProfielActivity.java
@@ -1,16 +1,44 @@
 package com.example.fitflex;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfielActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
+    EditText email;
+    EditText wachtwoord;
+    EditText telefoon;
+    EditText locatie;
+    EditText naam;
+
+    ImageView profiel_foto;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiel);
-    }
 
+        mAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.profiel_email);
+        wachtwoord = findViewById(R.id.profiel_wachtwoord);
+        telefoon = findViewById(R.id.profiel_telefoonnummer);
+        locatie = findViewById(R.id.profiel_locatie);
+        naam = findViewById(R.id.profiel_naam);
+
+        progressBar = findViewById(R.id.profiel_progressBar);
+        profiel_foto = findViewById(R.id.profiel_foto);
+
+    }
 
 }
